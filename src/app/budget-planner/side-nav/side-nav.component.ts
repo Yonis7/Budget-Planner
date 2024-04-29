ce85a38ecdf7bf3a664391de395f0fff8c6e816b
Yonis7 <yonisaden7@gmail.com>
2024-04-28 22:40:10 -0400

    Update side-nav component UI and styles, add slide-out panel functionality

--- src/app/budget-planner/side-nav/side-nav.component.ts
+++ src/app/budget-planner/side-nav/side-nav.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-side-nav',
   standalone: true,
@@ -11,9 +12,25 @@ export class SideNavComponent {
   // Set the initial value of the slide out to true, this will make the side nav visible when the page loads
   isSlideOut = true;
 
-  // Function to toggle the slide out. Inside the function, we are setting the value of isSlideOut to the opposite of its current value. This will make the side nav slide in and out when the button is clicked.
+  constructor(private router: Router) { }
+
+  // Function to toggle the slide out. Inside the function, we are setting the value of isSlideOut to the opposite of its current value. This will make the side nav slide in and out when the button is clicked. Void is used to indicate that the function does not return anything. It does not affect the functionality of the function.
   toggleSlideOut(): void {
     this.isSlideOut = !this.isSlideOut;
+  }
+
+  onDash() {
+    this.router.navigate(['/budget-planner/dashboard']);
+  }
+
+  onProfile() {
+    this.router.navigate(['/budget-planner/profile']);
+  }
+  onHistory() {
+    this.router.navigate(['/budget-planner/history']);
+  }
+  onLogout() {
+    this.router.navigate(['/login']);
   }
 
 }
