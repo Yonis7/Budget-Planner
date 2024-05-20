722934a1a1ab181a9e80a7226ac26b2408d2cd2e
Yonis7 <yonisaden7@gmail.com>
2024-05-20 16:02:09 -0400

    refactor: Update dashboard component UI and styles, add upcoming expenses and total income cards

--- src/app/budget-planner/dasbboard/dasbboard.component.ts
+++ src/app/budget-planner/dasbboard/dasbboard.component.ts
@@ -20,13 +20,34 @@ export class DasbboardComponent {
   LastMonthsExpense = ['January: $500', 'February: $1000', 'March: $1500'];
   currentMonthExpense = '$500';
 
+  //Upcoming expenses are displayed using the following variables.
+  upcomingExpenses = [
+    { description: 'Pay Rent', amount: '$500', date: '01/04/2021' },
+    { description: 'Buy Groceries', amount: '$100', date: '05/04/2021' },
+    { description: 'Pay Bills', amount: '$200', date: '10/04/2021' },
+    { description: 'Buy Clothes', amount: '$100', date: '15/04/2021' },
+  ];
+
+  totalCurrentMonthIncome = 2000;
+  totalCurrentMonthExpense = 1500;
+
   // The purpose of this constructor is to navigate to the income page. It is done by injecting the Router module.
   constructor(public router: Router) {}
 
+  // This method is executed when the user clicks on the button to view the income.
   onIncome() {
     this.router.navigate(['/budget-planner/income']);
   }
+  // This method is executed when the user clicks on the button to view the expenses.
   onExpense() {
     this.router.navigate(['/budget-planner/expense']);
+  }
+  // This method is executed when the user clicks on the button to view the upcoming expenses.
+  onUpcoming() {
+    this.router.navigate(['/budget-planner/upcoming']);
+  }
+  // gettter method to calculate the savings of the user. It is calculated by subtracting the total current month income from the total current month expense.
+  get currentMonthSavings(): number {
+    return this.totalCurrentMonthIncome - this.totalCurrentMonthExpense;
   }
 }
