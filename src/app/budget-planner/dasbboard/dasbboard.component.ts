046046f9e1ba756e7d5bf216bd868dda0f6143ef
Yonis7 <yonisaden7@gmail.com>
2024-05-20 15:33:49 -0400

    feat: Add income and expense cards to dashboard component Fixed buggs with positioning and styling.

--- src/app/budget-planner/dasbboard/dasbboard.component.ts
+++ src/app/budget-planner/dasbboard/dasbboard.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 import { SideNavComponent } from '../side-nav/side-nav.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dasbboard',
   standalone: true,
-  imports: [MatIconModule, SideNavComponent],
+  imports: [MatIconModule, SideNavComponent, CommonModule],
   templateUrl: './dasbboard.component.html',
-  styleUrl: './dasbboard.component.scss'
+  styleUrl: './dasbboard.component.scss',
 })
 export class DasbboardComponent {
+  // The following variables are used to display the income of the user.
+  LastMonthsIncome = ['January: $1000', 'February: $2000', 'March: $3000'];
+  currentMonthIncome = '$1000';
 
+  //Expense of the user is displayed using the following variables.
+  LastMonthsExpense = ['January: $500', 'February: $1000', 'March: $1500'];
+  currentMonthExpense = '$500';
+
+  // The purpose of this constructor is to navigate to the income page. It is done by injecting the Router module.
+  constructor(public router: Router) {}
+
+  onIncome() {
+    this.router.navigate(['/budget-planner/income']);
+  }
+  onExpense() {
+    this.router.navigate(['/budget-planner/expense']);
+  }
 }
