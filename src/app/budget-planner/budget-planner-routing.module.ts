e79fd629c6106f1da27a8d3ae2e98139726cfb60
Yonis7 <yonisaden7@gmail.com>
2024-05-20 21:40:09 -0400

    feat: Add income component to budget planner module

--- src/app/budget-planner/budget-planner-routing.module.ts
+++ src/app/budget-planner/budget-planner-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { DasbboardComponent } from './dasbboard/dasbboard.component';
+import { IncomeComponent } from './income/income.component';
 
 // These set of routes will be used to navigate between different components in the Budget Planner module. The routes array contains the path to the component and the component that should be rendered when the path is accessed. The routes array is then exported to be used in the BudgetPlannerRoutingModule class.
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'side-nav', component: SideNavComponent },
   { path: 'dashboard', component: DasbboardComponent },
+  { path: 'income', component: IncomeComponent },
 ];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
